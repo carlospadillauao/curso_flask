d9ca8621159035dd454813d40383b8778220767d
carlospadillauao <52969420+carlospadillauao@users.noreply.github.com>
2020-09-07 23:12:03 -0500

    Add files via upload

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class DevelopmentConfig(Config):
 
     #REQUERIDO OBLIGATORIO configuración de acceso para apps menos seguras
     MAIL_SERVER = 'smtp.googlemail.com'# mail_Server - servidor de correos a utiliozar    
-    MAIL_PORT = 587# indicar server /(tls) ????
+    MAIL_PORT = 587# indicar server /(tls) ???? Servidor de correo saliente SMTP​
     MAIL_USE_TLS = True # mail_server
     MAIL_USERNAME = 'carlos.padilla.tsakana@gmail.com' # mail_server
     MAIL_PASSWORD = 'TSAKANApadilla123' #canviar a variables de entorno / decouple >> libreria
